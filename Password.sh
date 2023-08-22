e0070ddc5461dd7235772faafae6297632c47e15
sup12123 <95585537+sup12123@users.noreply.github.com>
2023-08-22 14:40:20 -0400

    Create Password.sh

---
+++ Password.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#this is a script to input the universal password. can be found at /home/passworduniv.txt #
+
+echo "Please input a universal password. Make it a secure password to gain points"
+
+read
