984c5e3056e78a46f9c51231e3ca12c57513a200
sup12123 <95585537+sup12123@users.noreply.github.com>
2023-08-22 14:46:35 -0400

    Update Password.sh
    This takes a universal password and writes it to a file so future scripts don’t have to keep asking. Basically to take the retardism out of the scripts.

--- Password.sh
+++ Password.sh
@@ -4,4 +4,13 @@
 
 echo "Please input a universal password. Make it a secure password to gain points"
 
-read
+read univ-password
+
+
+echo univ-password >> /home/passworduniv.txt
+
+echo "the universal password has been changed to" cat >> /home/passworduniv.txt
+
+sleep 10
+
+echo "If you need to change or view the password, its located in /home/passworduniv.txt"
