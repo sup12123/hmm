d5b9fada5568a5cbe002532afec26299aa50aa74
sup12123 <95585537+sup12123@users.noreply.github.com>
2023-08-24 08:33:50 -0400

    Update The-big-one.sh

--- The-big-one.sh
+++ The-big-one.sh
@@ -2,4 +2,38 @@
 
 echo "this is the big one. follow all instructions carefully"
 
-wget 
+#downloads the scripts to /Downloads
+
+cd Downloads
+
+wget https://raw.githubusercontent.com/sup12123/hmm/main/poop.sh
+wget https://raw.githubusercontent.com/sup12123/hmm/main/CIS-benchmark-lazy-version.sh
+wget https://raw.githubusercontent.com/sup12123/hmm/main/Password.sh
+wget https://raw.githubusercontent.com/sup12123/hmm/main/Shikiller.sh
+wget https://raw.githubusercontent.com/sup12123/hmm/main/pamconf.sh
+wget https://raw.githubusercontent.com/sup12123/hmm/main/Grubsecure.sh
+
+#makes the scripts executable 
+
+cd Downloads
+
+sudo chmod +x poop.sh CIS-benchmark-lazy-version.sh Password.sh Shitkiller.sh pamconf.sh
+
+#execution time 
+
+sudo ./Password.sh
+sudo ./CIS-benchmark-lazy-version.sh
+sudo ./shitkiller.sh
+sudo ./pamconf.sh
+sudo ./Grubsecure.sh
+
+#running updates, upgrades ,and reboot.
+
+sudo apt update
+sudo apt upgrade
+sudo apt autoremove 
+
+sudo reboot
+
+
+
